038ef55d0f41148d971988bf52c17dd46b54aed0
Mr.TaiKe <117250841+MrTaiKe@users.noreply.github.com>
2022-11-20 13:36:03 +0800

    For Openwrt v22.03.2 Built

--- opwrt-22.03.2/Opwrt-p1_feeds.sh
+++ opwrt-22.03.2/Opwrt-p1_feeds.sh
@@ -5,7 +5,7 @@
 # 添加插件源码 Add a feed source
 #echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 # Add Theme package
 rm -rf package/luci-theme-argon
